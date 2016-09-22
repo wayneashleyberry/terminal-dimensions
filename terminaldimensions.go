c81bfda2b4e2cb26f4308bb130dd31b070bc546b
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-09-22 18:25:40 +0200

    adds height function

--- terminaldimensions.go
+++ terminaldimensions.go
@@ -7,16 +7,27 @@ import (
 	"strings"
 )
 
-// Width gives you the width of the terminal
-func Width() (uint, error) {
+func parts() []string {
 	cmd := exec.Command("stty", "size")
 	cmd.Stdin = os.Stdin
-	out, outputErr := cmd.Output()
-	if outputErr != nil {
-		return 0, outputErr
-	}
-	parts := strings.Split(string(out), " ")
+	out, _ := cmd.Output()
+	return strings.Split(string(out), " ")
+}
+
+// Width gives you the width of the terminal
+func Width() (uint, error) {
+	parts := parts()
 	x, err := strconv.Atoi(strings.Replace(parts[1], "\n", "", -1))
+	if err != nil {
+		return 0, err
+	}
+	return uint(x), err
+}
+
+// Height gives you the height of the terminal
+func Height() (uint, error) {
+	parts := parts()
+	x, err := strconv.Atoi(parts[0])
 	if err != nil {
 		return 0, err
 	}
