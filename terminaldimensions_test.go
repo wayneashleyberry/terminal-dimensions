77586b37e256d916b840634c0cebdd0f4d069ab5
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-09-23 09:50:02 +0200

    go vet fixes

--- terminaldimensions_test.go
+++ terminaldimensions_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestParser(t *testing.T) {
 	x, _, _ := parse("123 456\n")
 	if x != uint(123) {
-		t.Errorf("Expected: %f Got: %f", uint(123), x)
+		t.Errorf("Expected: %d Got: %d", uint(123), x)
 	}
 }
