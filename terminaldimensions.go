ad6bdba4e0710900744134490db4d55b2e93f877
Wayne Ashley Berry <727262+wayneashleyberry@users.noreply.github.com>
2021-10-12 07:32:45 +0100

    updates linting config

--- terminaldimensions.go
+++ terminaldimensions.go
@@ -12,20 +12,25 @@ import (
 func size() (string, error) {
 	cmd := exec.Command("stty", "size")
 	cmd.Stdin = os.Stdin
+
 	out, err := cmd.Output()
+
 	return string(out), err
 }
 
 func parse(input string) (uint, uint, error) {
 	parts := strings.Split(input, " ")
+
 	x, err := strconv.Atoi(parts[0])
 	if err != nil {
 		return 0, 0, err
 	}
+
 	y, err := strconv.Atoi(strings.Replace(parts[1], "\n", "", 1))
 	if err != nil {
 		return 0, 0, err
 	}
+
 	return uint(x), uint(y), nil
 }
 
@@ -50,7 +55,9 @@ func Width() (uint, error) {
 	if err != nil {
 		return 0, err
 	}
+
 	_, width, err := parse(output)
+
 	return width, err
 }
 
@@ -60,6 +67,8 @@ func Height() (uint, error) {
 	if err != nil {
 		return 0, err
 	}
+
 	height, _, err := parse(output)
+
 	return height, err
 }
